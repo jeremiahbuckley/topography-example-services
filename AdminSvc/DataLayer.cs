776ee42d58d9cf0a023bea1255e19f31c668432f
Jeremiah Buckley <jeremiah.buckley@gmail.com>
2017-11-02 23:38:15 -0400

    filled out data layer

--- AdminSvc/DataLayer.cs
+++ AdminSvc/DataLayer.cs
@@ -24,12 +24,13 @@ namespace AdminSvc
 			db = new SqlDatabase(connStr);
 		}
 
+		#region Topic
 		public int TopicCreate(string name, bool enabled)
 		{
 			var cmd = db.GetStoredProcCommand("Topic_Insert");
 			db.DiscoverParameters(cmd);
-			cmd.Parameters["@name"].Value = name;
-			cmd.Parameters["@enabled"].Value = enabled;
+			cmd.Parameters["name"].Value = name;
+			cmd.Parameters["enabled"].Value = enabled;
 
 			var id = (int)db.ExecuteScalar(cmd);
 
@@ -42,23 +43,23 @@ namespace AdminSvc
 			db.DiscoverParameters(cmd);
 			if (id.HasValue)
 			{
-				cmd.Parameters["@id"].Value = id.Value;
+				cmd.Parameters["id"].Value = id.Value;
 			}
 
-			var topics = new List<Topic>();
+			var results = new List<Topic>();
 			using (var reader = db.ExecuteReader(cmd))
 			{
 				while (reader.Read())
 				{
-					Topic t = new Topic();
-					t.Id = reader.GetInt32(reader.GetOrdinal("Id"));
-					t.Name = reader.GetString(reader.GetOrdinal("Name"));
-					t.Enabled = reader.GetBoolean(reader.GetOrdinal("Enabled"));
-					topics.Add(t);
+					var result = new Topic();
+					result.Id = reader.GetInt32(reader.GetOrdinal("Id"));
+					result.Name = reader.GetString(reader.GetOrdinal("Name"));
+					result.Enabled = reader.GetBoolean(reader.GetOrdinal("Enabled"));
+					results.Add(result);
 				}
 			}
 
-			return topics;
+			return results;
 		}
 
 		public IList<Topic> TopicReadAll()
@@ -70,9 +71,9 @@ namespace AdminSvc
 		{
 			var cmd = db.GetStoredProcCommand("Topic_Update");
 			db.DiscoverParameters(cmd);
-			cmd.Parameters["@id"].Value = id;
-			cmd.Parameters["@name"].Value = name;
-			cmd.Parameters["@enabled"].Value = enabled;
+			cmd.Parameters["id"].Value = id;
+			cmd.Parameters["name"].Value = name;
+			cmd.Parameters["enabled"].Value = enabled;
 
 			int updateId = -1;
 			using (var reader = db.ExecuteReader(cmd))
@@ -90,9 +91,263 @@ namespace AdminSvc
 		{
 			var cmd = db.GetStoredProcCommand("Topic_Delete");
 			db.DiscoverParameters(cmd);
-			cmd.Parameters["@id"].Value = id;
+			cmd.Parameters["id"].Value = id;
 			db.ExecuteNonQuery(cmd);
 		}
+		#endregion
 
+		#region Thread
+		public int ThreadCreate(string name, int topicId, bool enabled, bool pinned, int? pinOrder)
+		{
+			var cmd = db.GetStoredProcCommand("Thread_Insert");
+			db.DiscoverParameters(cmd);
+			cmd.Parameters["name"].Value = name;
+			cmd.Parameters["pinned"].Value = pinned;
+			cmd.Parameters["topicid"].Value = topicId;
+			cmd.Parameters["enabled"].Value = enabled;
+			
+			if (pinOrder.HasValue)
+			{
+				cmd.Parameters["pinorder"].Value = pinOrder;
+			}
+
+			var id = (int)db.ExecuteScalar(cmd);
+
+			return id;
+		}
+
+		public IList<Thread> ThreadRead(int? id)
+		{
+			var cmd = db.GetStoredProcCommand("Thread_Select");
+			db.DiscoverParameters(cmd);
+			if (id.HasValue)
+			{
+				cmd.Parameters["id"].Value = id.Value;
+			}
+
+			var results = new List<Thread>();
+			using (var reader = db.ExecuteReader(cmd))
+			{
+				while (reader.Read())
+				{
+					var result = new Thread();
+					result.Id = reader.GetInt32(reader.GetOrdinal("Id"));
+					result.Name = reader.GetString(reader.GetOrdinal("Name"));
+					result.Enabled = reader.GetBoolean(reader.GetOrdinal("Enabled"));
+					result.Pinned = reader.GetBoolean(reader.GetOrdinal("Pinned"));
+					if (!reader.IsDBNull(reader.GetOrdinal("PinOrder")))
+					{
+						result.PinOrder = reader.GetInt32(reader.GetOrdinal("PinOrder"));
+					}
+					results.Add(result);
+				}
+			}
+
+			return results;
+		}
+
+		public IList<Thread> ThreadReadAll()
+		{
+			return ThreadRead(null);
+		}
+
+		public int ThreadUpdate(int id, string name, int topicId, bool enabled, bool pinned, int? pinOrder)
+		{
+			var cmd = db.GetStoredProcCommand("Thread_Update");
+			db.DiscoverParameters(cmd);
+			cmd.Parameters["id"].Value = id;
+			cmd.Parameters["name"].Value = name;
+			cmd.Parameters["pinned"].Value = pinned;
+			cmd.Parameters["topicid"].Value = topicId;
+			cmd.Parameters["enabled"].Value = enabled;
+			if (pinOrder.HasValue)
+			{
+				cmd.Parameters["pinorder"].Value = pinOrder;
+			}
+
+			int updateId = -1;
+			using (var reader = db.ExecuteReader(cmd))
+			{
+				while (reader.Read())
+				{
+					id = reader.GetInt32(reader.GetOrdinal("Id"));
+				}
+			}
+
+			return updateId;
+		}
+
+		public void ThreadDelete(int id)
+		{
+			var cmd = db.GetStoredProcCommand("Thread_Delete");
+			db.DiscoverParameters(cmd);
+			cmd.Parameters["id"].Value = id;
+			db.ExecuteNonQuery(cmd);
+		}
+		#endregion
+
+		#region User
+		public int UserCreate(string name, bool enabled)
+		{
+			var cmd = db.GetStoredProcCommand("User_Insert");
+			db.DiscoverParameters(cmd);
+			cmd.Parameters["name"].Value = name;
+			cmd.Parameters["enabled"].Value = enabled;
+
+			var id = (int)db.ExecuteScalar(cmd);
+
+			return id;
+		}
+
+		public IList<User> UserRead(int? id)
+		{
+			var cmd = db.GetStoredProcCommand("User_Select");
+			db.DiscoverParameters(cmd);
+			if (id.HasValue)
+			{
+				cmd.Parameters["id"].Value = id.Value;
+			}
+
+			var results = new List<User>();
+			using (var reader = db.ExecuteReader(cmd))
+			{
+				while (reader.Read())
+				{
+					var result = new User();
+					result.Id = reader.GetInt32(reader.GetOrdinal("Id"));
+					result.Name = reader.GetString(reader.GetOrdinal("Name"));
+					result.Enabled = reader.GetBoolean(reader.GetOrdinal("Enabled"));
+					result.DateTimeJoined = reader.GetDateTime(reader.GetOrdinal("DateTimeJoined"));
+					results.Add(result);
+				}
+			}
+
+			return results;
+		}
+
+		public IList<User> UserReadAll()
+		{
+			return UserRead(null);
+		}
+
+		public int UserUpdate(int id, string name, bool enabled)
+		{
+			var cmd = db.GetStoredProcCommand("User_Update");
+			db.DiscoverParameters(cmd);
+			cmd.Parameters["id"].Value = id;
+			cmd.Parameters["name"].Value = name;
+			cmd.Parameters["enabled"].Value = enabled;
+
+			int updateId = -1;
+			using (var reader = db.ExecuteReader(cmd))
+			{
+				while (reader.Read())
+				{
+					id = reader.GetInt32(reader.GetOrdinal("Id"));
+				}
+			}
+
+			return updateId;
+		}
+
+		public void UserDelete(int id)
+		{
+			var cmd = db.GetStoredProcCommand("User_Delete");
+			db.DiscoverParameters(cmd);
+			cmd.Parameters["id"].Value = id;
+			db.ExecuteNonQuery(cmd);
+		}
+		#endregion
+
+		#region Comment
+		public int CommentCreate(int topicId, int threadId, int userId, string comment, int? replyToCommentId)
+		{
+			var cmd = db.GetStoredProcCommand("Comment_Insert");
+			db.DiscoverParameters(cmd);
+			cmd.Parameters["topicid"].Value = topicId;
+			cmd.Parameters["threadid"].Value = threadId;
+			cmd.Parameters["userid"].Value = userId;
+			cmd.Parameters["comment"].Value = comment;
+			if (replyToCommentId.HasValue)
+			{
+				cmd.Parameters["replytocommentid"].Value = replyToCommentId;
+			}
+
+			var id = (int)db.ExecuteScalar(cmd);
+
+			return id;
+		}
+
+		public IList<Comment> CommentRead(int? id)
+		{
+			var cmd = db.GetStoredProcCommand("Comment_Select");
+			db.DiscoverParameters(cmd);
+			if (id.HasValue)
+			{
+				cmd.Parameters["id"].Value = id.Value;
+			}
+
+			var results = new List<Comment>();
+			using (var reader = db.ExecuteReader(cmd))
+			{
+				while (reader.Read())
+				{
+					var result = new Comment();
+					result.Id = reader.GetInt32(reader.GetOrdinal("Id"));
+					result.TopicId = reader.GetInt32(reader.GetOrdinal("TopicId"));
+					result.ThreadId = reader.GetInt32(reader.GetOrdinal("ThreadId"));
+					result.UserId = reader.GetInt32(reader.GetOrdinal("UserId"));
+					if (!reader.IsDBNull(reader.GetOrdinal("ReplyToCommentId")))
+					{
+						result.ReplyToCommentId = reader.GetInt32(reader.GetOrdinal("ReplyToCommentId"));
+					}
+					result.CommentStr = reader.GetString(reader.GetOrdinal("Comment"));
+					result.DateTimeAdded = reader.GetDateTime(reader.GetOrdinal("DateTimeAdded"));
+					results.Add(result);
+				}
+			}
+
+			return results;
+		}
+
+		public IList<Comment> CommentReadAll()
+		{
+			return CommentRead(null);
+		}
+
+		public int CommentUpdate(int id, int topicId, int threadId, int userId, string comment, int? replyToCommentId)
+		{
+			var cmd = db.GetStoredProcCommand("Comment_Update");
+			db.DiscoverParameters(cmd);
+			cmd.Parameters["id"].Value = id;
+			cmd.Parameters["topicid"].Value = topicId;
+			cmd.Parameters["threadid"].Value = threadId;
+			cmd.Parameters["userid"].Value = userId;
+			cmd.Parameters["comment"].Value = comment;
+			if (replyToCommentId.HasValue)
+			{
+				cmd.Parameters["replytocommentid"].Value = replyToCommentId;
+			}
+
+			int updateId = -1;
+			using (var reader = db.ExecuteReader(cmd))
+			{
+				while (reader.Read())
+				{
+					id = reader.GetInt32(reader.GetOrdinal("Id"));
+				}
+			}
+
+			return updateId;
+		}
+
+		public void CommentDelete(int id)
+		{
+			var cmd = db.GetStoredProcCommand("Comment_Delete");
+			db.DiscoverParameters(cmd);
+			cmd.Parameters["id"].Value = id;
+			db.ExecuteNonQuery(cmd);
+		}
+		#endregion
 	}
 }
--- SvcInterfaces/TransportObjects.cs
+++ SvcInterfaces/TransportObjects.cs
@@ -11,6 +11,7 @@ namespace SvcInterfaces
 		public int Id { get; set; }
 		public int TopicId { get; set; }
 		public int ThreadId { get; set; }
+		public int UserId { get; set; }
 		public int? ReplyToCommentId { get; set; }
 		public string CommentStr { get; set; }
 		public DateTime DateTimeAdded { get; set; }
@@ -18,6 +19,7 @@ namespace SvcInterfaces
 		public Comment ReplyToComment { get; set; }
 		public Thread Thread { get; set; }
 		public Topic Topic { get; set; }
+		public User User { get; set; }
 	}
 
 	public class Thread
